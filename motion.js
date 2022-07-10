e118c2f8cbecd9fabecc1ff7bfe1bee3f05864b8
Your Name <you@example.com>
2022-07-10 15:04:27 -0400

    added function to send events

--- motion.js
+++ motion.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 9800;
 const WebSocket = require("ws");
 const gpio = require("rpi-gpio");
 
-const ws = new WebSocket("ws://192.168.1.118:9800/");
+const ws = new WebSocket(process.env.WEBSOCKET_SERVER_ADDRESS);
 
 ws.on("open", function open() {
   console.log("Homebridge connection open.");
@@ -14,37 +15,37 @@ ws.on("message", function incoming(data) {
   console.log(data);
 });
 
+const sendMotionEvent = ({ value, name, characteristic }) => {
+  ws.send(
+    JSON.stringify({
+      topic: "setValue",
+      payload: {
+        name,
+        characteristic,
+        value,
+      },
+    })
+  );
+};
+
 gpio.on("change", function (channel, value) {
   if (value) {
-    console.log("Motion detected")
-    ws.send(
-      JSON.stringify({
-        topic: "setValue",
-        payload: {
-          name: "WorkbenchMotionSensor",
-          characteristic: "MotionDetected",
-          value: true,
-        },
-      })
-    );
-  }
-  else{
-    console.log("No motion detected")
-    ws.send(
-      JSON.stringify({
-        topic: "setValue",
-        payload: {
-          name: "WorkbenchMotionSensor",
-          characteristic: "MotionDetected",
-          value: false,
-        },
-      })
-    );
+    console.log("Motion detected");
+    sendMotionEvent({
+      name: process.env.MOTION_SENSOR_ID,
+      characteristic: "MotionDetected",
+      value: true,
+    });
+  } else {
+    console.log("No motion detected");
+    sendMotionEvent({
+      name: process.env.MOTION_SENSOR_ID,
+      characteristic: "MotionDetected",
+      value: false,
+    });
   }
 });
 
-gpio.setup(11, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(process.env.GPIO_PIN_ID, gpio.DIR_IN, gpio.EDGE_BOTH);
 
-app.listen(port, () =>
-  console.log(`Motion Sensor running on 192.168.1.118:${port}`)
-);
+app.listen(port, () => console.log(`Motion Sensor running`));
