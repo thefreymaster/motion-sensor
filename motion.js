b205f050e0197a235fd5cd78238f501b93ca085b
Your Name <you@example.com>
2022-07-10 14:53:14 -0400

    working motion sensor

--- motion.js
+++ motion.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 9800;
 const WebSocket = require("ws");
-const gpio = require("gpio");
+const gpio = require("rpi-gpio");
 
 const ws = new WebSocket("ws://192.168.1.118:9800/");
 
@@ -14,42 +14,36 @@ ws.on("message", function incoming(data) {
   console.log(data);
 });
 
-var PIN = gpio.export(11, {
-  direction: gpio.DIRECTION.OUT,
-  interval: 200,
-  ready: function () {
-    PIN.on("change", function (val) {
-      console.log(val);
-      //   setTimeout(() => {
-      //     console.log(PIN.value);
-      //     if (PIN.value === 1) {
-      //       // statusLed.write(led.ON);
-      //       ws.send(
-      //         JSON.stringify({
-      //           topic: "setValue",
-      //           payload: {
-      //             name: "WorkbenchMotionSensor",
-      //             characteristic: "MotionDetected",
-      //             value: true,
-      //           },
-      //         })
-      //       );
-      //     } else {
-      //       ws.send(
-      //         JSON.stringify({
-      //           topic: "setValue",
-      //           payload: {
-      //             name: "WorkbenchMotionSensor",
-      //             characteristic: "MotionDetected",
-      //             value: false,
-      //           },
-      //         })
-      //       );
-      //     }
-      //   }, 300);
-    });
-  },
+gpio.on("change", function (channel, value) {
+  if (value) {
+    console.log("Motion detected")
+    ws.send(
+      JSON.stringify({
+        topic: "setValue",
+        payload: {
+          name: "WorkbenchMotionSensor",
+          characteristic: "MotionDetected",
+          value: true,
+        },
+      })
+    );
+  }
+  else{
+    console.log("No motion detected")
+    ws.send(
+      JSON.stringify({
+        topic: "setValue",
+        payload: {
+          name: "WorkbenchMotionSensor",
+          characteristic: "MotionDetected",
+          value: false,
+        },
+      })
+    );
+  }
 });
+
+gpio.setup(11, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 app.listen(port, () =>
   console.log(`Motion Sensor running on 192.168.1.118:${port}`)
